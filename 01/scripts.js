2bdc260250616c57c2388c2143631e1ac2fa8012
Damian Molinski <tedeer@gmail.com>
2021-05-31 15:07:52 +0200

    Start lern

---
+++ 01/scripts.js
@@ -0,0 +1,8 @@
+var a = 1;
+var b = 2;
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+console.log(add(a,b));
---
+++ 02/add.js
@@ -0,0 +1,13 @@
+var message = "Moduł add.js";
+
+console.log(message);
+
+
+var a = 1;
+var b = 2;
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+console.log(add(a,b));
